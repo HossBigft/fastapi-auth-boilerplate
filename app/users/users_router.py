8d57cf157747b205d3f90f3b8087a757fc2c2a14
Maxim <m.glushchenko@office.hoster.kz>
2025-09-04 19:16:28 +0500

    Add auth to the user endpoints

--- app/users/users_router.py
+++ app/users/users_router.py
@@ -30,6 +30,7 @@ router = APIRouter(tags=["users"], prefix="/users")
 
 @router.get(
     "/",
+    dependencies=[Depends(get_current_active_superuser)],
     response_model=UsersPublic,
 )
 def read_users(session: SessionDep, skip: int = 0, limit: int = 100) -> Any:
