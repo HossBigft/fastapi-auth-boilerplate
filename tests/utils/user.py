60f16a1a6f6badfc88b635894aced69178fdad45
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-09-06 11:44:47 +0500

    remove ssh realted code form tests

--- tests/utils/user.py
+++ tests/utils/user.py
@@ -2,7 +2,7 @@ from httpx import AsyncClient
 from sqlalchemy.orm import Session
 
 from app.db import crud
-from app.schemas import UserUpdateMePassword, UserCreate, UserUpdate
+from app.schemas import UserUpdateMePassword, UserCreate
 from tests.utils.utils import random_email, random_lower_string
 
 
@@ -26,25 +26,3 @@ def create_random_user(db: Session) -> UserUpdateMePassword:
     return user
 
 
-async def authentication_token_from_email(
-    *, client: AsyncClient, email: str, db: Session
-) -> dict[str, str]:
-    """
-    Return a valid token for the user with given email.
-
-    If the user doesn't exist it is created first.
-    """
-    password = random_lower_string()
-    user = crud.get_user_by_email(session=db, email=email)
-    if not user:
-        user_in_create = UserCreate(email=email, password=password)
-        user = crud.create_user(session=db, user_create=user_in_create)
-    else:
-        user_in_update = UserUpdate(password=password)
-        if not user.id:
-            raise Exception("User id not set")
-        user = crud.update_user(session=db, db_user=user, user_in=user_in_update)
-
-    return await user_authentication_headers(
-        client=client, email=email, password=password
-    )
