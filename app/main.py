07cdd0a81e093729250819bc8684c76a162ab36c
Maxim <m.glushchenko@office.hoster.kz>
2025-09-08 10:07:57 +0500

    Remove scripts

--- app/main.py
+++ app/main.py
@@ -1,37 +1,20 @@
-import sentry_sdk
-
 from fastapi import FastAPI, APIRouter
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
 
 from app.core.config import settings
-from app.core_utils.loggers import LoggingMiddleware
 from app.users import users_router as users
 from app.auth import auth_router as login
-from app.api import utils_router as utils
-from app.core_utils.loggers import disable_default_uvicorn_access_logs
 
 
 def custom_generate_unique_id(route: APIRoute) -> str:
     return f"{route.tags[0]}-{route.name}"
 
 
-if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
-    sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    disable_default_uvicorn_access_logs()
-    yield
-
-
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
-    lifespan=lifespan,
 )
 
 # Set all CORS enabled origins
@@ -44,11 +27,19 @@ if settings.all_cors_origins:
         allow_headers=["*"],
     )
 
-app.add_middleware(LoggingMiddleware)
 api_router = APIRouter(prefix=settings.API_V1_STR)
 
 api_router.include_router(users.router)
-api_router.include_router(utils.router)
 api_router.include_router(login.router)
 
 app.include_router(api_router)
+
+health_router = APIRouter(tags=["healthcheck"])
+
+
+@health_router.get("/health-check/")
+async def health_check() -> bool:
+    return True
+
+
+app.include_router(health_router)
